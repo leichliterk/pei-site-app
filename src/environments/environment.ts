c80193991c015c13eee47e2f4a97d05626b87f79
Kent Leichliter <leichliterk@gmail.com>
2026-02-11 20:50:50 -0600

    Update version info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   staging: false,
-  version: '1.0.2',
+  version: '1.1.0',
   appName: 'PEI Site App',
-  siteNumber: 1051,
-  siteName: 'Development Site',
+  siteNumber: 1052,
+  siteName: 'Dev Site 2',
   apiUrl: 'http://localhost:443/api/data',
   tenantId: 1001,
   apiKey: '_6@L<Q*SC?mSdp$a1E4?L{"M+8QQ0|Cw'
