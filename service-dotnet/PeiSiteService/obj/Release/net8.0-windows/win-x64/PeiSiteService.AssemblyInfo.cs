661c4097385339739ff7a963f6492df258a466e7
Kent Leichliter <leichliterk@gmail.com>
2026-02-18 21:25:18 -0600

    V1.3.5 build

--- service-dotnet/PeiSiteService/obj/Release/net8.0-windows/win-x64/PeiSiteService.AssemblyInfo.cs
+++ service-dotnet/PeiSiteService/obj/Release/net8.0-windows/win-x64/PeiSiteService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pei-site-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dac3b0edb73cfc09718ab4ad5992eb4fbb7c8243")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+539360618d451953d2be91a43421427b8a63f6a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("pei-site-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pei-site-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
