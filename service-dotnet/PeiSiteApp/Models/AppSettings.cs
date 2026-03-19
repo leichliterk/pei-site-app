34ea907c22ac45f08faa2d14bcffb09c89d86dca
Kent Leichliter <leichliterk@gmail.com>
2026-03-18 21:19:21 -0500

    Update buid scripts to account for both env and version number

--- service-dotnet/PeiSiteApp/Models/AppSettings.cs
+++ service-dotnet/PeiSiteApp/Models/AppSettings.cs
@@ -6,9 +6,14 @@ public class AppSettings
     public string AppName { get; set; } = "PEI Site App";
     public int SiteNumber { get; set; } = 1000;
     public string SiteName { get; set; } = "";
+#if PRODUCTION
+    public string ApiUrl { get; set; } = "https://pei-web-server.onrender.com/api/data";
+    public bool Staging { get; set; } = false;
+#else
     public string ApiUrl { get; set; } = "https://pei-web-server-staging.onrender.com/api/data";
+    public bool Staging { get; set; } = true;
+#endif
     public int TenantId { get; set; } = 1001;
     public string ApiKey { get; set; } = "";
-    public bool Staging { get; set; } = true;
     public string LogLevel { get; set; } = "info";
 }
